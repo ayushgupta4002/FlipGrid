653e8be5f2bbe02cc08e614e5fc84c9ed3a53d80
Aditya <adityasinghboss1234@gmail.com>
2023-08-20 17:00:59 +0530

    Washing machine loader & GUPTA KI MKC

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -3,13 +3,12 @@
 import MainNav from "@/app/Components/MainNav";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { ProductType, SuggestedType , ProductDescription } from "@/utils/types";
+import { ProductType, SuggestedType, ProductDescription } from "@/utils/types";
 import axios from "axios";
 import SuggestedCard from "@/app/Components/SuggestedCard";
+import LoadingWashingMachine from "@/app/Components/LoadingWashingMachine";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
-
 
 const productData: ProductType[] = [
   {
@@ -86,50 +85,58 @@ const productData: ProductType[] = [
   },
 ];
 
-
-const ProductData : ProductDescription[] = [
+const ProductData: ProductDescription[] = [
   {
-    "item": "Anarkali Kurti",
-    "description": "Elegance meets tradition with our exquisite Anarkali Kurti. Crafted from rich, flowing fabric, this piece showcases intricate embroidery that weaves tales of timeless beauty. The delicate patterns dance along the hem and cuffs, a testament to the skilled craftsmanship behind every stitch. Wear it with grace and let the world marvel at your regal presence."
+    item: "Anarkali Kurti",
+    description:
+      "Elegance meets tradition with our exquisite Anarkali Kurti. Crafted from rich, flowing fabric, this piece showcases intricate embroidery that weaves tales of timeless beauty. The delicate patterns dance along the hem and cuffs, a testament to the skilled craftsmanship behind every stitch. Wear it with grace and let the world marvel at your regal presence.",
   },
   {
-    "item": "Synthetic Sandals",
-    "description": "Step into comfort and style with our Synthetic Sandals. Designed to caress your feet in softness, these sandals effortlessly marry form and function. The contoured sole provides support with each step, while the sleek straps embrace your feet like a second skin. Whether strolling through the city streets or lounging by the beach, these sandals are your perfect companions."
+    item: "Synthetic Sandals",
+    description:
+      "Step into comfort and style with our Synthetic Sandals. Designed to caress your feet in softness, these sandals effortlessly marry form and function. The contoured sole provides support with each step, while the sleek straps embrace your feet like a second skin. Whether strolling through the city streets or lounging by the beach, these sandals are your perfect companions.",
   },
   {
-    "item": "Oversized Tshirt",
-    "description": "Embrace the art of laid-back chic with our Oversized Tshirt. It's not just a garment; it's a statement. The generous cut exudes a carefree vibe, while the fine fabric embraces you in comfort. With a versatile design, you can wear it as a dress or pair it with your favorite jeans for an effortlessly cool look that's as unique as you are."
+    item: "Oversized Tshirt",
+    description:
+      "Embrace the art of laid-back chic with our Oversized Tshirt. It's not just a garment; it's a statement. The generous cut exudes a carefree vibe, while the fine fabric embraces you in comfort. With a versatile design, you can wear it as a dress or pair it with your favorite jeans for an effortlessly cool look that's as unique as you are.",
   },
   {
-    "item": "High Rise Jeans",
-    "description": "Reach for the sky and your fashion aspirations with our High Rise Jeans. Designed to accentuate your curves and elevate your style, these jeans offer a flattering fit that's as comfortable as it is captivating. The high waist elongates your silhouette, while the quality denim ensures durability and lasting appeal. Embrace the confidence that comes with the perfect pair of jeans."
+    item: "High Rise Jeans",
+    description:
+      "Reach for the sky and your fashion aspirations with our High Rise Jeans. Designed to accentuate your curves and elevate your style, these jeans offer a flattering fit that's as comfortable as it is captivating. The high waist elongates your silhouette, while the quality denim ensures durability and lasting appeal. Embrace the confidence that comes with the perfect pair of jeans.",
   },
   {
-    "item": "Round Neck Sweater",
-    "description": "Wrap yourself in warmth and sophistication with our Round Neck Sweater. As the temperature drops, your style rises with this cozy companion. The soft, luxurious knit envelops you in comfort, while the timeless round neck design lends versatility to your wardrobe. Whether layered over a collared shirt or worn on its own, this sweater is the epitome of timeless charm."
+    item: "Round Neck Sweater",
+    description:
+      "Wrap yourself in warmth and sophistication with our Round Neck Sweater. As the temperature drops, your style rises with this cozy companion. The soft, luxurious knit envelops you in comfort, while the timeless round neck design lends versatility to your wardrobe. Whether layered over a collared shirt or worn on its own, this sweater is the epitome of timeless charm.",
   },
   {
-    "item": "Running Shoes",
-    "description": "Unleash your inner athlete with our Running Shoes. Engineered for performance, these shoes are designed to carry you beyond limits. The responsive sole provides cushioning with every stride, while the breathable mesh upper ensures optimal airflow. Lace up these companions of speed and hit the track, letting the wind be your competition and the finish line your victory."
+    item: "Running Shoes",
+    description:
+      "Unleash your inner athlete with our Running Shoes. Engineered for performance, these shoes are designed to carry you beyond limits. The responsive sole provides cushioning with every stride, while the breathable mesh upper ensures optimal airflow. Lace up these companions of speed and hit the track, letting the wind be your competition and the finish line your victory.",
   },
   {
-    "item": "Rugged Cargo Pant",
-    "description": "Embark on an adventure of style and utility with our Rugged Cargo Pant. Crafted for the modern explorer, these pants offer an abundance of pockets for all your essentials. From urban escapades to off-road journeys, the durable fabric and sturdy construction ensure that you're prepared for whatever the day brings. Wherever you go, let your pants tell the story of your fearless spirit."
+    item: "Rugged Cargo Pant",
+    description:
+      "Embark on an adventure of style and utility with our Rugged Cargo Pant. Crafted for the modern explorer, these pants offer an abundance of pockets for all your essentials. From urban escapades to off-road journeys, the durable fabric and sturdy construction ensure that you're prepared for whatever the day brings. Wherever you go, let your pants tell the story of your fearless spirit.",
   },
   {
-    "item": "Party Wear Boots",
-    "description": "Make a grand entrance with our Party Wear Boots. These boots aren't just footwear; they're a work of art that adorns your feet. With intricate detailing and a sleek silhouette, they're the perfect accomplice for your evenings of enchantment. The heel adds a touch of elegance to your stride, ensuring that all eyes are on you as you step onto the dance floor."
+    item: "Party Wear Boots",
+    description:
+      "Make a grand entrance with our Party Wear Boots. These boots aren't just footwear; they're a work of art that adorns your feet. With intricate detailing and a sleek silhouette, they're the perfect accomplice for your evenings of enchantment. The heel adds a touch of elegance to your stride, ensuring that all eyes are on you as you step onto the dance floor.",
   },
   {
-    "item": "Denim Shirt",
-    "description": "Channel rugged sophistication with our Denim Shirt. The classic denim fabric gets a modern twist in this versatile piece. Whether layered over a graphic tee or buttoned up for a polished look, this shirt adapts to your mood and the occasion. It's more than clothing; it's an expression of your individuality and an ode to the timeless charm of denim."
-  }
+    item: "Denim Shirt",
+    description:
+      "Channel rugged sophistication with our Denim Shirt. The classic denim fabric gets a modern twist in this versatile piece. Whether layered over a graphic tee or buttoned up for a polished look, this shirt adapts to your mood and the occasion. It's more than clothing; it's an expression of your individuality and an ode to the timeless charm of denim.",
+  },
 ];
-
 
 export default function ProductDesc(params: any) {
   const [cart, setCart] = useState<ProductType[]>([]);
   const [pastProduct, setPastProduct] = useState<SuggestedType[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const cartString = localStorage.getItem("cart");
@@ -158,7 +165,7 @@ export default function ProductDesc(params: any) {
     (product) => encodeURIComponent(product.item) === item
   );
 
-  const Productdesc  = ProductData.find(
+  const Productdesc = ProductData.find(
     (product) => encodeURIComponent(product.item) === item
   );
 
@@ -168,9 +175,11 @@ export default function ProductDesc(params: any) {
       .then((response) => {
         const data = response.data;
         setPastProduct(Object.values(data));
+        setLoading(false);
       })
       .catch((error) => {
         console.log("Error in fetching data", error);
+        setLoading(false);
       });
   }, [selectedProduct]);
 
@@ -188,7 +197,8 @@ export default function ProductDesc(params: any) {
               {selectedProduct?.item}
             </div>
             <div className="text-2xl  pt-[3vh] font-sans	font-normal pl-[3vw] pr-[3vw] ">
-              <span className="font-semibold">Description</span> : {Productdesc?.description}
+              <span className="font-semibold">Description</span> :{" "}
+              {Productdesc?.description}
             </div>
             <div className="text-2xl  pt-[3vh] font-sans	font-normal pl-[3vw] pr-[3vw] ">
               <span className="font-semibold">Price</span> : ₹{" "}
@@ -230,9 +240,13 @@ export default function ProductDesc(params: any) {
       <div className="flex flex-row justify-center flex-wrap">
         <div className="flex flex-row">
           <div className=" flex flex-row flex-wrap justify-center items-center my-7 max-w-[63vw] ml-5">
-            {pastProduct.map((product) => {
-              return <SuggestedCard key={product.Name} product={product} />;
-            })}
+            {loading ? (
+              <LoadingWashingMachine />
+            ) : (
+              pastProduct.map((product) => {
+                return <SuggestedCard key={product.Name} product={product} />;
+              })
+            )}
           </div>
         </div>
       </div>
